1aa58f755fd266147dc2374878cd6ebc336b0e18
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-16 01:48:28 +0600

    updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl">Evenly</h1>
-      <Button variant="destructive">Hello</Button>
+     
     </main>
   );
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,9 +1,18 @@
- import React from 'react'
- 
- const Header = () => {
-   return (
-     <div>Header</div>
-   )
- }
- 
- export default Header
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="w-full border bottom-1 ">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image src="/assets/images/logo.svg" 
+          alt="Evently logo"  width={128} height={38}/>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
