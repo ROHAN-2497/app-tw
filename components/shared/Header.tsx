1e42bb25726c6c08123ac90b7af779b20e42f9d7
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-16 15:36:02 +0600

    auth token

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,15 +1,28 @@
+import { SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
 
 const Header = () => {
   return (
     <header className="w-full border bottom-1 ">
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
-          <Image src="/assets/images/logo.svg" 
-          alt="Evently logo"  width={128} height={38}/>
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Evently logo"
+            width={128}
+            height={38}
+          />
         </Link>
+        <div>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   );
