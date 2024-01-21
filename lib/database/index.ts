1f581f979ebd90619a0e01ca4aac9b3c8fee4861
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-21 23:44:33 +0600

    up

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,2 +1,9 @@
 import mongoose from 'mongoose'
-let cahed = mongoose || { conn: null, promise: null }
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+export const connectToDatabase = () => {
+    if (cached.conn) return cached.conn;
+}
