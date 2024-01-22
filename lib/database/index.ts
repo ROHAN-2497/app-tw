e1a8ee239d26c81cc8c4c2ccc3b0f36f75f8c1cb
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-22 16:44:09 +0600

    up

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -4,6 +4,8 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
+
+
 export const connectToDatabase = () => {
     if (cached.conn) return cached.conn;
 }
