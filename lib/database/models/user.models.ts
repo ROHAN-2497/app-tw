a4086adcde542a0429465bdd638a087ae2a92b5d
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-23 07:37:24 +0600

    create a user Schema

---
+++ lib/database/models/user.models.ts
@@ -0,0 +1,23 @@
+import { Schema, model, models } from "mongoose"
+
+const userSchema = new Schema({
+    clerkId: {
+        type: String, required: true, unique: true
+    },
+    email: {
+        type: String, required: true, unique: true,
+    },
+    username: { type: String, required: true, unique: true },
+    firstName: {
+        type: String, required: true
+    },
+    lastName: {
+        type: String, required: true
+    },
+    photo: {
+        type: true, required: true
+    }
+})
+
+const User = models.user || model('user', userSchema)
+export default User
