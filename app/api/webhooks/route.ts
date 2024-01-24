e62a5a66201ec485c4ab112b076874280cd4fafd
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-24 10:23:06 +0600

    up

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -4,6 +4,7 @@ import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser } from '@/lib/actions/user.actions'
 import { clearTraceEvents } from 'next/dist/trace/trace'
 import { clerkClient } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
 
@@ -66,20 +67,43 @@ export async function POST(req: Request) {
         }
 
         const newUser = await createUser(user);
-        if(newUser){
+        if (newUser) {
             await clerkClient.users.updateUserMetadata(id, {
-                publicMetadata:{
+                publicMetadata: {
                     userId: newUser._id
                 }
             })
+            return NextResponse.json({ message: 'OK', user: newUser })
         }
     }
+
+
+    if (eventType === 'user.updated') {
+        const { id, image_url, first_name, last_name, username } = evt.data
+
+        const user = {
+            firstName: first_name,
+            lastName: last_name,
+            username: username!,
+            photo: image_url,
+        }
+
+        const updatedUser = await updateUser(id, user)
+
+        return NextResponse.json({ message: 'OK', user: updatedUser })
+    }
+
+
+    if (eventType === 'user.deleted') {
+        const { id } = evt.data
+
+        const deletedUser = await deleteUser(id!)
+        return NextResponse.json({ message: 'OK', user: deletedUser })
+    }
+
+
 
 
     return new Response('', { status: 200 })
 }
 
-
-
-// console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-// console.log('Webhook body:', body)
