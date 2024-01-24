844cc2ac8b6d8b62ec7e2bf713ed04d2c3f8d53d
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-24 22:13:50 +0600

    up

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,8 +1,8 @@
+
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
-import { createUser } from '@/lib/actions/user.actions'
-import { clearTraceEvents } from 'next/dist/trace/trace'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
 
@@ -54,29 +54,31 @@ export async function POST(req: Request) {
     // Get the ID and type
     const { id } = evt.data;
     const eventType = evt.type;
+
     if (eventType === 'user.created') {
         const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+
         const user = {
             clerkId: id,
             email: email_addresses[0].email_address,
             username: username!,
             firstName: first_name,
             lastName: last_name,
-            photo: image_url
-
+            photo: image_url,
         }
 
         const newUser = await createUser(user);
+
         if (newUser) {
             await clerkClient.users.updateUserMetadata(id, {
                 publicMetadata: {
                     userId: newUser._id
                 }
             })
-            return NextResponse.json({ message: 'OK', user: newUser })
         }
-    }
 
+        return NextResponse.json({ message: 'OK', user: newUser })
+    }
 
     if (eventType === 'user.updated') {
         const { id, image_url, first_name, last_name, username } = evt.data
@@ -93,17 +95,37 @@ export async function POST(req: Request) {
         return NextResponse.json({ message: 'OK', user: updatedUser })
     }
 
-
     if (eventType === 'user.deleted') {
         const { id } = evt.data
 
         const deletedUser = await deleteUser(id!)
+
         return NextResponse.json({ message: 'OK', user: deletedUser })
     }
 
-
-
-
     return new Response('', { status: 200 })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
